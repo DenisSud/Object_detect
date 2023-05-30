07e34faead75fdd75e4e560c2d53fbb39761b394
DenisSud <sudakov.denis.2007@gmail.com>
2023-05-30 20:16:50 +0300

    nothing

--- main.py
+++ main.py
@@ -28,30 +28,3 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
-#v2
-"""import cv2
-import numpy as np
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
-import visualization
-
-# settup
-base_options = python.BaseOptions(model_asset_path='efficientdet_lite0.tflite')
-options = vision.ObjectDetectorOptions(base_options=base_options, score_threshold=0.5)
-detector = vision.ObjectDetector.create_from_options(options)
-
-#load camera
-video = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = video.read()
-    if ret:
-        detection_result = detector.detect(frame, image_format=cv2.COLOR_BGR2RGB)
-        annotated_image = visualization.visualize(frame, detection_result)
-        cv2.imshow("detect", annotated_image)
-        key = cv2.waitKey(10)
-        if key == ord("q"):
-            break
-
-"""
